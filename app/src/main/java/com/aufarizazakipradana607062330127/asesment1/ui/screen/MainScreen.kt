326d882081e2ccec8cef9c2a2278862755a29d25
Aufa Riza Zaki Pradana <aufa.rzp10@gmail.com>
2025-03-24 00:44:35 +0700

    Meminta input data dari pengguna

--- app/src/main/java/com/aufarizazakipradana607062330127/asesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aufarizazakipradana607062330127/asesment1/ui/screen/MainScreen.kt
@@ -1,17 +1,30 @@
 package com.aufarizazakipradana607062330127.asesment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aufarizazakipradana607062330127.asesment1.R
 import com.aufarizazakipradana607062330127.asesment1.ui.theme.Asesment1Theme
 
@@ -37,10 +50,56 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+    var namaMerek by remember { mutableStateOf("") }
+    var harga by remember { mutableStateOf("") }
+    var stok by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.manajemen_barang_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = namaMerek,
+            onValueChange = { namaMerek = it },
+            label = { Text(text = stringResource(R.string.nama_merek)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = harga,
+            onValueChange = { harga = it },
+            label = { Text(text = stringResource(R.string.harga)) },
+            trailingIcon = { Text(text = "Rp") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = stok,
+            onValueChange = { stok = it },
+            label = { Text(text = stringResource(R.string.stok)) },
+            trailingIcon = { Text(text = "Item") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+    }
 }
 
 
