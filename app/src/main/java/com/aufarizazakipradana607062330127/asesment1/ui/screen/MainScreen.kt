3509bc85b81cfd5829a8a00dbcf00c9bb9a853c8
Aufa Riza Zaki Pradana <aufa.rzp10@gmail.com>
2025-03-24 01:22:48 +0700

    Membuat Button Simpan (No saving)

--- app/src/main/java/com/aufarizazakipradana607062330127/asesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aufarizazakipradana607062330127/asesment1/ui/screen/MainScreen.kt
@@ -3,10 +3,12 @@ package com.aufarizazakipradana607062330127.asesment1.ui.screen
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -22,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -70,6 +73,7 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.fillMaxWidth()
         )
+        // Form Nama Merek
         OutlinedTextField(
             value = namaMerek,
             onValueChange = { namaMerek = it },
@@ -81,6 +85,7 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
+        // Form Harga
         OutlinedTextField(
             value = harga,
             onValueChange = { harga = it },
@@ -93,6 +98,7 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
+        // Form Stok
         OutlinedTextField(
             value = stok,
             onValueChange = { stok = it },
@@ -105,6 +111,7 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
+        // Dropdown Menu
         ExposedDropdownMenuBox(
             expanded = expanded,
             onExpandedChange = { expanded = !expanded }
@@ -135,9 +142,16 @@ fun ScreenContent(modifier: Modifier = Modifier) {
                 }
             }
         }
+        // Tombol Simpan
+        Button(
+            onClick = {},
+            modifier = Modifier.padding(top = 8.dp).align(Alignment.CenterHorizontally),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+            Text(text = stringResource(R.string.simpan))
+        }
     }
 }
-
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
