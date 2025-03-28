c569f8f52dd4cdcbf07898e5931efb853da0baf2
Aufa Riza Zaki Pradana <aufa.rzp10@gmail.com>
2025-03-28 15:07:53 +0700

    Membuat Form User dan Hasil Simpan

--- app/src/main/java/com/aufarizazakipradana607062330127/asesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aufarizazakipradana607062330127/asesment1/ui/screen/MainScreen.kt
@@ -4,8 +4,10 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -26,9 +28,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aufarizazakipradana607062330127.asesment1.R
@@ -62,10 +67,15 @@ fun ScreenContent(modifier: Modifier = Modifier) {
     var stok by remember { mutableStateOf("") }
     var expanded by remember { mutableStateOf(false) }
     var pilihanKategori by remember { mutableStateOf("Pilih kategori") }
+    var pesanErrorNama by remember { mutableStateOf("") }
+    var pesanErrorHarga by remember { mutableStateOf("") }
+    var pesanErrorStok by remember { mutableStateOf("") }
+    var simpanData by remember { mutableStateOf<List<String>>(emptyList()) }
     val kategori = listOf(R.string.lampu, R.string.kipas_angin, R.string.speaker)
 
     Column(
         modifier = modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         Text(
@@ -73,10 +83,14 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.fillMaxWidth()
         )
+
         // Form Nama Merek
         OutlinedTextField(
             value = namaMerek,
-            onValueChange = { namaMerek = it },
+            onValueChange = {
+                namaMerek = it
+                pesanErrorNama = if (it.isBlank()) "Nama merek tidak boleh kosong" else ""
+            },
             label = { Text(text = stringResource(R.string.nama_merek)) },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
@@ -85,10 +99,19 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
+        if (pesanErrorNama.isNotEmpty()) {
+            Text(text = pesanErrorNama, color = Color.Red)
+        }
+
         // Form Harga
         OutlinedTextField(
             value = harga,
-            onValueChange = { harga = it },
+            onValueChange = {
+                if (it.all { char -> char.isDigit() }) {
+                    harga = it
+                }
+                pesanErrorHarga = if (harga.isBlank()) "Harga tidak boleh kosong" else ""
+            },
             label = { Text(text = stringResource(R.string.harga)) },
             trailingIcon = { Text(text = "Rp") },
             singleLine = true,
@@ -98,10 +121,19 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
+        if (pesanErrorHarga.isNotEmpty()) {
+            Text(text = pesanErrorHarga, color = Color.Red)
+        }
+
         // Form Stok
         OutlinedTextField(
             value = stok,
-            onValueChange = { stok = it },
+            onValueChange = {
+                if (it.all { char -> char.isDigit()}) {
+                    stok = it
+                }
+                pesanErrorStok = if (it.isBlank()) "Stok tidak boleh kosong" else ""
+            },
             label = { Text(text = stringResource(R.string.stok)) },
             trailingIcon = { Text(text = "Item") },
             singleLine = true,
@@ -111,6 +143,10 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
+        if (pesanErrorStok.isNotEmpty()) {
+            Text(text = pesanErrorStok, color = Color.Red)
+        }
+
         // Dropdown Menu
         ExposedDropdownMenuBox(
             expanded = expanded,
@@ -142,13 +178,42 @@ fun ScreenContent(modifier: Modifier = Modifier) {
                 }
             }
         }
+
         // Tombol Simpan
         Button(
-            onClick = {},
+            onClick = {
+                pesanErrorNama = if (namaMerek.isBlank()) "Nama merek tidak boleh kosong" else ""
+                pesanErrorHarga = if (harga.isBlank()) "Harga tidak boleh kosong" else ""
+                pesanErrorStok = if (stok.isBlank()) "Stok tidak boleh kosong" else ""
+
+                if (namaMerek.isNotBlank() && harga.isNotBlank() && stok.isNotBlank() && pilihanKategori != "Pilih kategori") {
+                    simpanData = simpanData + listOf(
+                        "Nama merek: $namaMerek\nHarga: $harga\nStok: $stok\nKategori: $pilihanKategori"
+                    )
+                    namaMerek = ""
+                    harga = ""
+                    stok = ""
+                    pilihanKategori = "Pilih kategori"
+
+                    pesanErrorNama = ""
+                    pesanErrorHarga = ""
+                    pesanErrorStok = ""
+                }
+            },
             modifier = Modifier.padding(top = 8.dp).align(Alignment.CenterHorizontally),
-            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+            contentPadding =  PaddingValues(horizontal = 32.dp, vertical = 16.dp)
         ) {
-            Text(text = stringResource(R.string.simpan))
+            Text(stringResource(R.string.simpan))
+        }
+
+        // Menampilkan hasil simpan
+        if (simpanData.isNotEmpty()) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Data yang disimpan:", fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+            simpanData.forEach { data ->
+                Text(text = data, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+                Spacer(modifier = Modifier.height(2.dp))
+            }
         }
     }
 }
