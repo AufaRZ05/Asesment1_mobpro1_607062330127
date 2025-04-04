b00c240c06253d8270c1c1d4ac8e4b8f33668d21
Aufa Riza Zaki Pradana <aufa.rzp10@gmail.com>
2025-04-04 18:07:49 +0700

    Membuat tampilan about

--- app/src/main/java/com/aufarizazakipradana607062330127/asesment1/navigation/Screen.kt
+++ app/src/main/java/com/aufarizazakipradana607062330127/asesment1/navigation/Screen.kt
@@ -2,4 +2,5 @@ package com.aufarizazakipradana607062330127.asesment1.navigation
 
 sealed class Screen(val route: String) {
     data object Home: Screen("mainScreen")
+    data object About: Screen("aboutScreen")
 }
